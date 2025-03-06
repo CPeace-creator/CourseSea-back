a481a8b092fd6c538c8f4bcf84708dd089f8e723
cjh <949661474@qq.com>
2025-03-06 17:09:30 +0800

    sql

--- db/course.sql
+++ db/course.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80100
  File Encoding         : 65001
 
- Date: 05/03/2025 17:14:39
+ Date: 06/03/2025 17:09:05
 */
 
 SET NAMES utf8mb4;
@@ -34,6 +34,12 @@ CREATE TABLE `category`  (
 -- ----------------------------
 -- Records of category
 -- ----------------------------
+INSERT INTO `category` VALUES (1, '前端开发', NULL, '2025-03-06 16:02:10', NULL, 0);
+INSERT INTO `category` VALUES (2, '后端开发', NULL, '2025-03-06 16:02:15', NULL, 0);
+INSERT INTO `category` VALUES (3, '运维部署', NULL, '2025-03-06 16:02:24', NULL, 0);
+INSERT INTO `category` VALUES (4, '移动开发', NULL, '2025-03-06 16:02:30', NULL, 0);
+INSERT INTO `category` VALUES (5, 'AI/机器学习', NULL, '2025-03-06 16:02:42', NULL, 0);
+INSERT INTO `category` VALUES (6, '其他', NULL, '2025-03-06 16:02:49', NULL, 0);
 
 -- ----------------------------
 -- Table structure for course
@@ -45,6 +51,7 @@ CREATE TABLE `course`  (
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `category_id` int(0) NULL DEFAULT 0,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `user_id` int(0) NULL DEFAULT NULL,
   `create_at` datetime(0) NULL DEFAULT NULL,
   `update_at` datetime(0) NULL DEFAULT NULL,
   `if_del` int(0) NULL DEFAULT 0,
@@ -73,7 +80,7 @@ CREATE TABLE `user`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `email`(`email`) USING BTREE,
   UNIQUE INDEX `user_name`(`user_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
